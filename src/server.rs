063b6bac837f724476e5773ece0b52d87983ee01
blobcode <lhelme123@gmail.com>
2022-04-20 14:11:49 -0400

    reduce buffer size

--- src/server.rs
+++ src/server.rs
@@ -37,8 +37,8 @@ impl Server {
 // request handler
 async fn handle(inbound: TcpStream, hosts: HashMap<String, String>) -> Result<(), Box<dyn Error>> {
     // buffer init
-    let mut buf = vec![0; 1024];
-    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut buf = vec![0; 512];
+    let mut headers = [httparse::EMPTY_HEADER; 8];
     let mut r = httparse::Request::new(&mut headers);
 
     // parse request
